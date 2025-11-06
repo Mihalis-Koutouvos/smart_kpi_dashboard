e011161b1ae05a1f27884bc1b40637949af67de3
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-11-06 18:57:52 -0500

    Pushed initial initializing code to github

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,19 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+#Initializing app using FastAPI
+app = FastAPI()
+
+#Permitting frontend requests; also adding in CORS permissions
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#Test route for running server
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
